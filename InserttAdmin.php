73789c10b2e7eb6fb8a2de92ab1359290741305d
MRyan210 <ryan.mudibo@strathmore.edu>
2023-07-27 19:29:27 +0300

    update 2

---
+++ InserttAdmin.php
@@ -0,0 +1,35 @@
+<?php
+$dbserver="localhost";
+$dbusername="root";
+$password="";
+$dbname="stockmgmt1";
+
+$connect=mysqli_connect($dbserver,$dbusername,$password,$dbname);
+
+
+$FirstName = $_POST["FirstName"];
+$LastName = $_POST["LastName"];
+$Email = $_POST["Email"];
+$DateofBirth = $_POST["DateofBirth"];
+$NationalID =$_POST["NationalID"];
+$Gender=$_POST["Gender"];
+$PhoneNumber = $_POST["PhoneNumber"];
+// $Password=$_POST["Password"];
+
+// $Password = sha1($Password);
+
+$insert="INSERT INTO `user`(`FirstName`, `LastName`, `NationalID`, `Gender`, `DateofBirth`, `Phonenumber`, `Email`,  `Role`, `Status`)
+VALUES('".$FirstName."','".$LastName."','".$NationalID."','".$Gender."','".$DateOfBirth."','".$PhoneNumber."','".$Email."','1','1')";
+
+
+if(mysqli_query($connect,$insert)){
+    echo "success";
+    header("Location: Admin- Manage Users.html");
+}
+else{
+    echo"failure";
+    echo"<br/>";
+    echo mysqli_error($connect);
+}
+
+?>
